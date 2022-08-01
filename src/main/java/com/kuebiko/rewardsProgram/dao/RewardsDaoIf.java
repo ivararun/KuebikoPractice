3eb46c3dec427fbedd970b78c1a179cd27c1f755
Arun Dhungana <arundhungana@Aruns-Macbook.local>
2022-08-01 11:29:45 -0500

    deleted old files

--- src/main/java/com/kuebiko/rewardsProgram/dao/RewardsDaoIf.java
+++
@@ -1,6 +0,0 @@
-package com.kuebiko.rewardsProgram.dao;
-
-public interface RewardsDaoIf {
-
-    public int computePoints(int purchaseAmount);
-}
--- src/main/java/com/kuebiko/rewardsProgram/service/RewardsServiceIf.java
+++
@@ -1,6 +0,0 @@
-package com.kuebiko.rewardsProgram.service;
-
-public interface RewardsServiceIf {
-
-    public int getPoints(int purchaseAmount);
-}
